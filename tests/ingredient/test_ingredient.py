fa2048c2d81d581ed4c129b666bc7509831a5441
Vinicius Soares <viniciussoares72943@gmail.com>
2023-05-16 14:14:19 -0300

    test: classe Ingredient

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,19 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 
-# Req 1 comeca project
+# Req 1
 def test_ingredient():
-    pass
+    ingredient_carne = Ingredient("carne")
+    ingredient_carne2 = Ingredient("carne")
+    ingredient_frango = Ingredient("frango")
+
+    assert ingredient_carne.__hash__() == ingredient_carne2.__hash__()
+    assert ingredient_frango.__hash__() != ingredient_carne.__hash__()
+    assert ingredient_carne.__eq__(ingredient_carne) is True
+    assert ingredient_carne.__eq__(ingredient_frango) is False
+    assert ingredient_carne.name == "carne"
+    assert ingredient_carne.__repr__() == "Ingredient('carne')"
+
+    assert Restriction.ANIMAL_MEAT in ingredient_carne.restrictions
+    assert Restriction.ANIMAL_DERIVED in ingredient_carne.restrictions
+
